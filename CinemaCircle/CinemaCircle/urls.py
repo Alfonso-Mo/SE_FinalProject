be502115450e143b7172d96e57b1555b8b01bc17
josec05 <115657851+josec05@users.noreply.github.com>
2026-08-07 04:16:28 +0000

    Webpage login changed color

--- CinemaCircle/CinemaCircle/urls.py
+++ CinemaCircle/CinemaCircle/urls.py
@@ -20,8 +20,9 @@ from django.contrib.auth import views as auth_views
 from movies import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('login/',auth_views.LoginView.as_view(template_name='movies/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='movies/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 
 ]
