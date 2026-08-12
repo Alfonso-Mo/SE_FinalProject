72323120aba9e9409fd75f53621b22778ff4c002
josec05 <115657851+josec05@users.noreply.github.com>
2026-08-12 04:55:42 +0000

    Added watch trailer

--- CinemaCircle/CinemaCircle/urls.py
+++ CinemaCircle/CinemaCircle/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='movies/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 
+    path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail'),
 ]
--- CinemaCircle/movies/views.py
+++ CinemaCircle/movies/views.py
@@ -5,15 +5,35 @@ def home(request):
     # Your TMDB API key 
     api_key = '0542151682e19c03fbe639039d90d8e9'
     
-    # The TMDB endpoint for movies currently playing in theaters
+    # Fetch Now Playing movies
     url = f"https://api.themoviedb.org/3/movie/now_playing?api_key={api_key}&language=en-US&page=1"
-    
-    # Makes the request to TMDB and convert the response to a Python dictionary
     response = requests.get(url)
     data = response.json()
-    
-    # Extract just the list of movies
     movies = data.get('results', [])
     
-    # Sends the movie list to your home.html template
     return render(request, 'movies/home.html', {'movies': movies})
+
+def movie_detail(request, movie_id):
+    # Your TMDB API key 
+    api_key = '0542151682e19c03fbe639039d90d8e9'
+    
+    # 1. Fetch specific movie details
+    movie_url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
+    movie_data = requests.get(movie_url).json()
+    
+    # 2. Fetch the movie's videos (Trailers)
+    videos_url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={api_key}&language=en-US"
+    videos_data = requests.get(videos_url).json()
+    
+    # 3. Find the YouTube trailer key
+    trailer_key = None
+    for video in videos_data.get('results', []):
+        if video.get('site') == 'YouTube' and video.get('type') == 'Trailer':
+            trailer_key = video.get('key')
+            break
+            
+    context = {
+        'movie': movie_data,
+        'trailer_key': trailer_key
+    }
+    return render(request, 'movies/movie_detail.html', context)
