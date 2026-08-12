71019fb72e6babcc336e37b8367c2554bb88e37d
josec05 <jose.cuevas05@utrgv.edu>
2026-08-12 16:30:37 -0500

    Added Watch Trailer feature with YouTube API integration and fixed TMDB data loading

--- CinemaCircle/movies/urls.py
+++ CinemaCircle/movies/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path
+from django.urls import path #added my code form codespaces
 from django.views.generic import RedirectView
-
 
 from . import views
 
@@ -10,4 +9,7 @@ urlpatterns = [
     path("login/", views.UserLoginView.as_view(), name="login"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("logout/", views.UserLogoutView.as_view(), name="logout"),
+    
+    # ADDED THIS LINE FOR THE WATCH TRAILER FEATURE:
+    path("movie/<int:movie_id>/", views.movie_detail, name="movie_detail"),
 ]
