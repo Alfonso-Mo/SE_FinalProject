b17be943138c968e0dc4fc4fe78f46b5d9b16e72
josec05 <115657851+josec05@users.noreply.github.com>
2026-08-12 05:13:21 +0000

    Added search, used ai to fix code for search to work

--- CinemaCircle/movies/views.py
+++ CinemaCircle/movies/views.py
@@ -2,16 +2,24 @@ import requests
 from django.shortcuts import render
 
 def home(request):
-    # Your TMDB API key 
     api_key = '0542151682e19c03fbe639039d90d8e9'
     
-    # Fetch Now Playing movies
-    url = f"https://api.themoviedb.org/3/movie/now_playing?api_key={api_key}&language=en-US&page=1"
+    # Grab the search term from the URL if it exists
+    query = request.GET.get('q')
+    
+    # If the user searched for something, use the Search API
+    if query:
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&language=en-US&query={query}&page=1"
+    # Otherwise, just show Now Playing
+    else:
+        url = f"https://api.themoviedb.org/3/movie/now_playing?api_key={api_key}&language=en-US&page=1"
+        
     response = requests.get(url)
     data = response.json()
     movies = data.get('results', [])
     
-    return render(request, 'movies/home.html', {'movies': movies})
+    # Send the movies AND the query back to the template
+    return render(request, 'movies/home.html', {'movies': movies, 'query': query})
 
 def movie_detail(request, movie_id):
     # Your TMDB API key 
